a00b2661bebffaa4669c2301ad6bb33085cbbeec
jwlato <jwlato@gmail.com>
2009-02-14 23:37:20 +0000

    move file_driver_rb to RandomIO.hs

--- src/Data/Iteratee.hs
+++ src/Data/Iteratee.hs
@@ -11,21 +11,4 @@ where
 import Data.Iteratee.Base
 import Data.Iteratee.Binary
 import Data.Iteratee.IO.RandomIO
-import Data.Word (Word8)
-import System.Posix
 
--- |Process a file using the given IterateeGM
-file_driver_rb :: ReadableChunk s Word8 => IterateeGM s Word8 IO a ->
-               FilePath ->
-               IO (Either (String, a) a)
-file_driver_rb iter filepath = do
-  fd <- openFd filepath ReadOnly Nothing defaultFileFlags
-  result <- unIM $ (enum_fd_random fd >. enum_eof) ==<< iter
-  closeFd fd
-  print_res result
- where
-  print_res (IE_done a (Err err)) = return $ Left (err, a)
-  print_res (IE_done a _) = return $ Right a
-  print_res (IE_cont _) = return $ Left ("Iteratee unfinished", undefined)
-  print_res (IE_jmp _ _) = return $ Left ("Iteratee unfinished", undefined)
-                           
--- src/Data/Iteratee/IO/RandomIO.hs
+++ src/Data/Iteratee/IO/RandomIO.hs
@@ -5,7 +5,8 @@
 module Data.Iteratee.IO.RandomIO (
   bindm,
   stakeR,
-  enum_fd_random
+  enum_fd_random,
+  file_driver_rb
 )
 
 where
@@ -68,4 +69,18 @@ enum_fd_random fd iter = {-# SCC "enum_fd_random" #-}
          im <- unIM $ step (Chunk s)
 	 loop (off + fromIntegral len,fromIntegral n') im p
 
-
+-- |Process a file using the given IterateeGM
+file_driver_rb :: ReadableChunk s Word8 => IterateeGM s Word8 IO a ->
+               FilePath ->
+               IO (Either (String, a) a)
+file_driver_rb iter filepath = do
+  fd <- openFd filepath ReadOnly Nothing defaultFileFlags
+  result <- unIM $ (enum_fd_random fd >. enum_eof) ==<< iter
+  closeFd fd
+  print_res result
+ where
+  print_res (IE_done a (Err err)) = return $ Left (err, a)
+  print_res (IE_done a _) = return $ Right a
+  print_res (IE_cont _) = return $ Left ("Iteratee unfinished", undefined)
+  print_res (IE_jmp _ _) = return $ Left ("Iteratee unfinished", undefined)
+                           

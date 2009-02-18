428d00290d2f8e182496a8e3f78ea9185d60b62c
jwlato <jwlato@gmail.com>
2009-02-18 00:35:58 +0000

    code cleanup in WrappedByteString files

--- src/Data/Iteratee/WrappedByteString.hs
+++ src/Data/Iteratee/WrappedByteString.hs
@@ -17,15 +17,15 @@ instance StreamChunk WrappedByteString Word8 where
   cLength        = BW.length . unWrap
   cEmpty         = WrapBS BW.empty
   cNull          = BW.null . unWrap
-  cCons a s      = WrapBS $ BW.cons a $ unWrap s
+  cCons a        = WrapBS . BW.cons a . unWrap
   cHead          = BW.head . unWrap
-  cTail s        = WrapBS (BW.tail $ unWrap s)
-  cFindIndex p s = BW.findIndex p $ unWrap s
+  cTail          = WrapBS . BW.tail . unWrap
+  cFindIndex p   = BW.findIndex p . unWrap
   cSplitAt i s   = let (a1, a2) = BW.splitAt i $ unWrap s
                    in (WrapBS a1, WrapBS a2)
-  cDropWhile p s = WrapBS (BW.dropWhile p $ unWrap s)
+  cDropWhile p   = WrapBS . BW.dropWhile p . unWrap
   cAppend a1 a2  = WrapBS (BW.append (unWrap a1) (unWrap a2))
-  fromList xs    = WrapBS (BW.pack xs)
+  fromList       = WrapBS . BW.pack
   toList         = BW.unpack . unWrap
   cMap           = bwmap
 
@@ -43,6 +43,6 @@ bwmap f xs = step xs
 bwmap' :: (Word8 -> Word8) ->
           WrappedByteString Word8 ->
           WrappedByteString Word8
-bwmap' f s = WrapBS (BW.map f $ unWrap s)
+bwmap' f = WrapBS . BW.map f . unWrap
 
 {-# RULES "bwmap/map" forall s (f :: Word8 -> Word8). bwmap f s = bwmap' f s #-}

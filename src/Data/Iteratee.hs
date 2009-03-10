b71893e794113676475b1bd6a602be4635434bb3
jwlato <jwlato@gmail.com>
2009-03-10 02:01:13 +0000

    Add header to Haddock docs.

--- src/Data/Iteratee.hs
+++ src/Data/Iteratee.hs
@@ -1,4 +1,8 @@
-{-# LANGUAGE FlexibleContexts #-}
+{- | Provide iteratee-based IO as described in Oleg Kiselyov's paper http://okmij.org/ftp/Haskell/Iteratee/.
+
+Oleg's original code uses lists to store buffers of data for reading in the iteratee.  This package allows the use of arbitrary types through use of the StreamChunk type class.  See Data.Iteratee.WrappedByteString for implementation details.
+
+-}
 
 module Data.Iteratee (
   module Data.Iteratee.Base,

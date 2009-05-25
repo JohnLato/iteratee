a6f8e8e31d4e7fb6c10efd7aef861e0e16dc6643
jwlato <jwlato@gmail.com>
2009-05-25 13:58:22 +0000

    add more tests (StreamG, isFinished, skipToEof)

--- tests/tests.hs
+++ tests/tests.hs
@@ -11,12 +11,39 @@ import Test.QuickCheck
 import Data.Iteratee hiding (head, break)
 import qualified Data.Iteratee as Iter
 import Control.Monad.Identity
+import Data.Monoid
 
 import Text.Printf (printf)
 import System.Environment (getArgs)
 
 instance Show (a -> b) where
   show _ = "<<function>>"
+
+-- ---------------------------------------------
+-- StreamG instances
+
+type ST = StreamG [] Int
+
+prop_mempty = mempty == (Chunk [] :: StreamG [] Int)
+
+prop_mappend str1 str2 | isChunk str1 && isChunk str2 =
+  str1 `mappend` str2 == Chunk (chunkData str1 ++ chunkData str2)
+prop_mappend str1 str2 = isEOF $ str1 `mappend` str2
+  where types = (str1 :: ST, str2 :: ST)
+
+prop_functor str@(EOF _) f = isEOF $ fmap f str
+prop_functor str@(Chunk xs) f = fmap f str == Chunk (fmap f xs)
+  where types = (str :: ST, f :: Int -> Integer)
+
+
+isChunk (Chunk _) = True
+isChunk (EOF _)   = False
+
+chunkData (Chunk xs) = xs
+
+isEOF (EOF _)   = True
+isEOF (Chunk _) = False
+
 
 -- ---------------------------------------------
 -- List <-> Stream
@@ -50,6 +77,9 @@ prop_peek xs = runner1 (enumPure1Chunk xs peek) == sHead xs
 prop_peek2 xs = runner1 (enumPure1Chunk xs (peek >> stream2list)) == xs
   where types = xs :: [Int]
 
+prop_skip xs = runner1 (enumPure1Chunk xs (skipToEof >> stream2list)) == []
+  where types = xs :: [Int]
+
 -- ---------------------------------------------
 -- Simple enumerator tests
 
@@ -80,18 +110,33 @@ prop_eof xs ys i = length xs > 0 ==>
                 == runner1 (enumPure1Chunk xs i)
   where types = (xs :: [Int], ys :: [Int], i :: I)
 
+prop_isFinished = runner1 (enumEof (isFinished :: IterateeG [] Int Identity (Maybe String))) == Just "EOF"
+
+prop_isFinished2 = runner1 (enumErr "Error" (isFinished :: IterateeG [] Int Identity (Maybe String))) == Just "Error"
+
+-- ---------------------------------------------
+-- Nested Iteratees
+
+-- take, mapStream, convStream, and takeR
+
 -- ---------------------------------------------
 tests = [
   testGroup "Elementary" [
     testProperty "list" prop_list
-    ,testProperty "chunkList" prop_clist],
-  testGroup "Simple Iteratees" [
+    ,testProperty "chunkList" prop_clist]
+  ,testGroup "StreamG tests" [
+    testProperty "mempty" prop_mempty
+    ,testProperty "mappend" prop_mappend
+    ,testProperty "functor" prop_functor
+  ]
+  ,testGroup "Simple Iteratees" [
     testProperty "break" prop_break
     ,testProperty "break2" prop_break2
     ,testProperty "head" prop_head
     ,testProperty "head2" prop_head2
     ,testProperty "peek" prop_peek
     ,testProperty "peek2" prop_peek2
+    ,testProperty "skipToEof" prop_skip
     ]
   ,testGroup "Simple Enumerators/Combinators" [
     testProperty "enumPureNChunk" prop_enumChunks
@@ -99,6 +144,8 @@ tests = [
     ,testProperty "enum sequencing" prop_app2
     ,testProperty "enum sequencing 2" prop_app3
     ,testProperty "enumEof" prop_eof
+    ,testProperty "isFinished" prop_isFinished
+    ,testProperty "isFinished error" prop_isFinished2
     ]
   ]
 

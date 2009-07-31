d028d0759173974f7bc61f75c04d88bf3f13bdaa
jwlato <jwlato@gmail.com>
2009-07-31 13:14:56 +0000

    new implementation of Handle enumerators Previously, Handle enumerators would only work in the IO monad.  This required users to frequently re-implement handle-based enumerators.
    The new design is polymorphic over any MonadIO intance, allowing for
    much greater flexibility.  Performance is generally similar to the
    old implementation.  In addition to passing the test suite, this
    implementation works properly for the iter-audio driver, which requires
    both random IO and StateT monad stack.

--- src/Data/Iteratee/IO/Handle.hs
+++ src/Data/Iteratee/IO/Handle.hs
@@ -22,9 +22,11 @@ import Data.Iteratee.Binary()
 
 import Data.Int
 import Control.Exception.Extensible
+import Control.Monad
+import Control.Monad.Trans
 
 import Foreign.Ptr
-import Foreign.Marshal.Alloc
+import Foreign.ForeignPtr
 import Foreign.Storable
 
 import System.IO
@@ -36,94 +38,101 @@ import System.IO
 -- |The enumerator of a file Handle.  This version enumerates
 -- over the entire contents of a file, in order, unless stopped by
 -- the iteratee.  In particular, seeking is not supported.
-enumHandle :: forall a s el.(ReadableChunk s el) => Handle -> EnumeratorGM s el IO a
-enumHandle h i = allocaBytes (fromIntegral buffer_size) $ loop i
+enumHandle :: forall s el m a.(ReadableChunk s el, MonadIO m) =>
+  Handle ->
+  EnumeratorGM s el m a
+enumHandle h i =
+  liftIO (mallocForeignPtrBytes (fromIntegral buffer_size)) >>= loop i
   where
-    buffer_size = 4096 - (mod 4096 $ sizeOf (undefined :: el))
-    loop iter p = do
-      n <- (try $ hGetBuf h p buffer_size) :: IO (Either SomeException Int)
-      case n of
-        Left _   -> enumErr "IO error" iter
-        Right 0  -> return iter
-        Right n' -> do
-          s <- readFromPtr p (fromIntegral n')
-          igv <- runIter iter (Chunk s)
-          check p igv
+    buffer_size = 4096 - mod 4096 (sizeOf (undefined :: el))
+    loop iter fp = do
+      s <- liftIO . withForeignPtr fp $ \p -> do
+        n <- try $ hGetBuf h p buffer_size :: IO (Either SomeException Int)
+        case n of
+          Left _  -> return $ Left "IO error"
+          Right 0 -> return $ Right Nothing
+          Right n' -> liftM (Right . Just) $ readFromPtr p (fromIntegral n')
+      checkres fp iter s
+    checkres fp iter = either (flip enumErr iter)
+                              (maybe (return iter)
+                                     (check fp <=< runIter iter . Chunk))
     check _p (Done x _) = return . return $ x
     check p  (Cont i' Nothing) = loop i' p
     check _p (Cont _ (Just e)) = return $ throwErr e
 
-
 -- |The enumerator of a Handle: a variation of enumHandle that
 -- supports RandomIO (seek requests)
-enumHandleRandom :: forall a s el.(ReadableChunk s el) => Handle -> EnumeratorGM s el IO a
-enumHandleRandom h iter =
- allocaBytes (fromIntegral buffer_size) (loop (0,0) iter)
+enumHandleRandom :: forall s el m a.(ReadableChunk s el, MonadIO m) =>
+  Handle ->
+  EnumeratorGM s el m a
+enumHandleRandom h i =
+ liftIO (mallocForeignPtrBytes (fromIntegral buffer_size)) >>= loop (0,0) i
  where
-  -- this can be usefully varied.  Values between 512 and 4096 seem
-  -- to provide the best performance for most cases.
-  buffer_size = 4096 - (mod 4096 $ sizeOf (undefined :: el))
+  buffer_size = 4096 - mod 4096 (sizeOf (undefined :: el))
   -- the first argument of loop is (off,len), describing which part
-  -- of the file is currently in the buffer 'p'
-{-
-  loop :: (ReadableChunk s el) =>
-          (FileOffset,Int) ->
-          IterateeG s el IO a ->
-          Ptr el ->
-          IO (IterateeG s el IO a)
--}
-  -- Otherwise, the `off + fromIntegral len' below accumulates thunks
-  loop (off,len) _iter' _p | off `seq` len `seq` False = undefined
-  loop (off,len) iter' p = do
-   n <- (try $ hGetBuf h p buffer_size) :: IO (Either SomeException Int)
-   case n of
-    Left _errno -> enumErr "IO error" iter
-    Right 0 -> return iter'
-    Right n' -> do
-         s <- readFromPtr p (fromIntegral n')
-         igv <- runIter iter' (Chunk s)
-         check (off + fromIntegral len,fromIntegral n') p igv
-  seekTo pos@(off, len) off' iter' p
+  -- of the file is currently in the buffer 'fp'
+  loop :: (FileOffset,Int) ->
+          IterateeG s el m a ->
+          ForeignPtr el ->
+          m (IterateeG s el m a)
+  -- strictify `off', else the `off + fromIntegral len' accumulates thunks
+  loop (off,len) _iter _p | off `seq` len `seq` False = undefined
+  loop (off,len) iter fp = do
+    s <- liftIO . withForeignPtr fp $ \p -> do
+      n <- try $ hGetBuf h p buffer_size :: IO (Either SomeException Int)
+      case n of
+        Left _errno -> return $ Left "IO error"
+        Right 0 -> return $ Right Nothing
+        Right n' -> liftM
+          (Right . Just . (,) (off + fromIntegral len, fromIntegral n'))
+          (readFromPtr p (fromIntegral n'))
+    checkres fp iter s
+  seekTo pos@(off, len) off' iter fp
     | off <= off' && off' < off + fromIntegral len =    -- Seek within buffer
     do
     let local_off = fromIntegral $ off' - off
-    s <- readFromPtr (p `plusPtr` local_off) (len - local_off)
-    igv <- runIter iter' (Chunk s)
-    check pos p igv
-  seekTo _pos off iter' p = do                          -- Seek outside buffer
-   off' <- (try $ hSeek h AbsoluteSeek
-            (fromIntegral off)) :: IO (Either SomeException ())
+    s <- liftIO $ withForeignPtr fp $ \p ->
+                    readFromPtr (p `plusPtr` local_off) (len - local_off)
+    igv <- runIter iter (Chunk s)
+    check pos fp igv
+  seekTo _pos off iter fp = do                          -- Seek outside buffer
+   off' <- liftIO (try $ hSeek h AbsoluteSeek
+            (fromIntegral off) :: IO (Either SomeException ()))
    case off' of
-    Left _errno -> enumErr "IO error" iter'
-    Right _     -> loop (off,0) iter' p
-  check _ _ (Done x _)                 = return . return $ x
-  check o p (Cont i Nothing)           = loop o i p
-  check o p (Cont i (Just (Seek off))) = seekTo o off i p
-  check _ _ (Cont _ (Just e))          = return $ throwErr e
+    Left _errno -> enumErr "IO error" iter
+    Right _     -> loop (off,0) iter fp
+  checkres fp iter = either
+                       (flip enumErr iter)
+                       (maybe (return iter) (uncurry $ runS fp iter))
+  runS fp iter o s = runIter iter (Chunk s) >>= check o fp
+  check _ _ (Done x _)                   = return . return $ x
+  check o fp (Cont i' Nothing)           = loop o i' fp
+  check o fp (Cont i' (Just (Seek off))) = seekTo o off i' fp
+  check _ _ (Cont _ (Just e))            = return $ throwErr e
 
 -- ----------------------------------------------
 -- File Driver wrapper functions.
 
 -- |Process a file using the given IterateeGM.  This function wraps
 -- enumHandle as a convenience.
-fileDriverHandle :: ReadableChunk s el =>
-                    IterateeG s el IO a ->
-                    FilePath ->
-                    IO a
+fileDriverHandle :: (MonadIO m, ReadableChunk s el) =>
+  IterateeG s el m a ->
+  FilePath ->
+  m a
 fileDriverHandle iter filepath = do
-  h <- openBinaryFile filepath ReadMode
+  h <- liftIO $ openBinaryFile filepath ReadMode
   result <- enumHandle h iter >>= run
-  hClose h
+  liftIO $ hClose h
   return result
 
 -- |Process a file using the given IterateeGM.  This function wraps
 -- enumHandleRandom as a convenience.
-fileDriverRandomHandle :: ReadableChunk s el =>
-                          IterateeG s el IO a ->
+fileDriverRandomHandle :: (MonadIO m, ReadableChunk s el) =>
+                          IterateeG s el m a ->
                           FilePath ->
-                          IO a
+                          m a
 fileDriverRandomHandle iter filepath = do
-  h <- openBinaryFile filepath ReadMode
+  h <- liftIO $ openBinaryFile filepath ReadMode
   result <- enumHandleRandom h iter >>= run
-  hClose h
+  liftIO $ hClose h
   return result

96594d107880831346c2a0bfccb115c4eb417a51
jwlato <jwlato@gmail.com>
2010-04-23 06:13:34 +0000

    update hpc script

--- tests/make_hpc.sh
+++ tests/make_hpc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ghc --make -i -i../src -hide-package iteratee -hide-package mtl -odir . -hidir . testIteratee.hs QCUtils.hs -fhpc
+ghc --make -i -i../src -hide-package iteratee -hide-package mtl -hide-package MonadCatchIO-mtl -odir . -hidir . testIteratee.hs QCUtils.hs -fhpc

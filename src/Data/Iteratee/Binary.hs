0dc5cec970d06b78c23c751059ec0d05c9dee7e4
jwlato <jwlato@gmail.com>
2010-05-28 12:45:45 +0000

    add endianRead3i

--- src/Data/Iteratee/Binary.hs
+++ src/Data/Iteratee/Binary.hs
@@ -7,6 +7,7 @@ module Data.Iteratee.Binary (
   -- * Endian multi-byte iteratees
   ,endianRead2
   ,endianRead3
+  ,endianRead3i
   ,endianRead4
 )
 where
@@ -55,10 +56,29 @@ endianRead3 e = do
     MSB -> return $ (((fromIntegral c1
                         `shiftL` 8) .|. fromIntegral c2)
                         `shiftL` 8) .|. fromIntegral c3
+    LSB -> return $ (((fromIntegral c3
+                        `shiftL` 8) .|. fromIntegral c2)
+                        `shiftL` 8) .|. fromIntegral c1
+
+-- |Read 3 bytes in an endian manner.  If the first bit is set (negative),
+-- set the entire first byte so the Int32 will be negative as
+-- well.
+endianRead3i
+  :: (Nullable s, LL.ListLike s Word8, Monad m) =>
+     Endian
+     -> Iteratee s m Int32
+endianRead3i e = do
+  c1 <- I.head
+  c2 <- I.head
+  c3 <- I.head
+  case e of
+    MSB -> return $ (((fromIntegral c1
+                        `shiftL` 8) .|. fromIntegral c2)
+                        `shiftL` 8) .|. fromIntegral c3
     LSB ->
      let m :: Int32
-         m = shiftR (shiftL (fromIntegral c3) 24) 8 in
-     return $ (((fromIntegral c3
+         m = shiftR (shiftL (fromIntegral c3) 24) 8
+     in return $ (((fromIntegral c3
                         `shiftL` 8) .|. fromIntegral c2)
                         `shiftL` 8) .|. fromIntegral m
 

647eae082ec193dafa8f2b721769058604108917
jwlato <jwlato@gmail.com>
2010-08-23 23:08:46 +0000

    minor changes to word.hs example

--- Examples/word.hs
+++ Examples/word.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 -- A simple wc-like program using Data.Iteratee
 module Main where
 
@@ -7,11 +9,12 @@ import Data.Iteratee.Char as C
 import qualified Data.Iteratee as I
 import qualified Data.ByteString.Char8 as BC
 import Data.Word
+import Data.Char
 import Data.ListLike as LL
 import System
 
 
--- An iteratee to calculate the number of characters in a stream.  Very basic.
+-- An iteratee to calculate the number of characters in a stream.  Very basic, assumes ASCII.
 numChars :: (Monad m, ListLike s el) => I.Iteratee s m Int
 numChars = I.length
 
@@ -28,10 +31,16 @@ numWords = I.joinI $ enumWordsBS I.length
 numLines :: Monad m => I.Iteratee BC.ByteString m Int
 numLines = I.joinI $ enumLinesBS I.length
 
+-- |A much more efficient numLines using the foldl' iteratee.
+numLines2 :: Monad m => I.Iteratee BC.ByteString m Int
+numLines2 = I.foldl' step 0
+ where
+  step !acc el = if el == (fromIntegral $ ord '\n') then acc + 1 else acc
+
 allIter :: Monad m => I.Iteratee BC.ByteString m ((Int, Int), Int)
 allIter = numLines `I.enumPair` numWords `I.enumPair` numChars
 
 main = do
   f:_ <- getArgs
-  words <- fileDriverVBuf (2^16) allIter f
+  words <- fileDriverVBuf 65536 numLines2 f
   print words
